6332c1bdc76fac8df06dcef2cccd589a58e43e17
Maeva ALBRAND <malbrand@e1r12p10.42.fr>
2021-06-23 05:11:09 +0200

    new printf

--- ft_based.c
+++ ft_based.c
@@ -1,44 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_based.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: malbrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/03 10:32:05 by malbrand          #+#    #+#             */
-/*   Updated: 2021/06/23 05:10:50 by malbrand         ###   ########.fr       */
+/*   Created: 2021/06/23 04:32:51 by malbrand          #+#    #+#             */
+/*   Updated: 2021/06/23 04:36:26 by malbrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printf(const char *str, ...)
+void	ft_putchar(char c)
 {
-	va_list param;
-	int		i;
-	int		ret;
-
-	va_start(param, str);
-	i = 0;
-	ret = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == '%')
-		{
-			return (0);
-		}
-		ft_putchar(str[i]);
-		ret = ret + 1;
-		i++;
-	}
-	va_end(param);
-	return (ret);
-}
-
-int main ()
-{
-	int ret; 
-
-	ret = ft_printf("rouge\n");
-	printf("ret = %d\n", ret);
+	write(1, &c, 1);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -1,44 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: malbrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/03/03 10:32:05 by malbrand          #+#    #+#             */
-/*   Updated: 2021/06/23 05:10:50 by malbrand         ###   ########.fr       */
+/*   Created: 2021/06/23 04:20:45 by malbrand          #+#    #+#             */
+/*   Updated: 2021/06/23 05:10:54 by malbrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-int ft_printf(const char *str, ...)
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+typedef struct		s_param
 {
-	va_list param;
-	int		i;
-	int		ret;
+	char			flag;
+}					t_param;
 
-	va_start(param, str);
-	i = 0;
-	ret = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == '%')
-		{
-			return (0);
-		}
-		ft_putchar(str[i]);
-		ret = ret + 1;
-		i++;
-	}
-	va_end(param);
-	return (ret);
-}
+void	ft_putchar(char c);
 
-int main ()
-{
-	int ret; 
-
-	ret = ft_printf("rouge\n");
-	printf("ret = %d\n", ret);
-}
+#endif
